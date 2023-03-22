90c987e0ad79ac21bc2fa9a955da912cdc6e3db9
maazjavaid <muhammad.maaz@mergestack.com>
2023-03-22 15:57:10 +0500

    Code refactored -used separate states for task and edit id -removed isEditable key -refactored css on edit  fields -removed isedited from reducers

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,48 +1,67 @@
-import React, { useState } from 'react'
-import { useDispatch} from 'react-redux'
-import { v4 } from 'uuid'
-import { addTask} from '../redux/tasks'
-import TaskList from './TaskList'
-import './tasks.css'
-const Tasks = () => {  
-  const dispatch=useDispatch()
-  const uid=v4()
-  const [input,setInput]=useState({
-    id:uid,
-    task:'',
-    completed:false,
-    isEdited:false
-  })
-  
-  const dispatchFunction=()=>{
-    dispatch(addTask(input))
-    setInput((prev)=>{
-        return{
-            ...prev,
-            id:v4(),
-        }
-    })
-  }
-  return (
-    <div className='task-container'>
-        <h1>Task Todo List</h1>
-    <div className='task-input'>
-        <input type="text" 
-        value={input.task}
-        onChange={(e)=>{
-            setInput((prev)=>{
-                return{
-                    ...prev,
-                    task:e.target.value
-                }
-            })
-        }}
-        />
-        <button onClick={()=>dispatchFunction()}>Add</button>
-    </div>
-    <TaskList/>
-    </div>
-  )
-}
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { v4 } from "uuid";
+import { addTask } from "../redux/tasks";
+import TaskList from "./TaskList";
+import "./tasks.css";
+const Tasks = () => {
+  const tasks = useSelector((state) => state.tasks);
+  const dispatch = useDispatch();
+  const uid = v4();
+  const [input, setInput] = useState({
+    task: "",
+  });
+  const [editId, setEditId] = useState(null);
 
-export default Tasks
+  return (
+    <div className="task-container">
+      <h1>Task Todo List</h1>
+
+      <div className="task-input">
+        <input
+          type="text"
+          disabled={editId}
+          value={editId ? "" : input.task}
+          onChange={(e) => {
+            setInput((prev) => {
+              return {
+                task: e.target.value,
+              };
+            });
+          }}
+        />
+
+        <button
+          disabled={editId}
+          onClick={() => {
+            dispatch(
+              addTask({
+                ...input,
+                completed: false,
+                id: uid,
+              })
+            );
+
+            setInput((prev) => {
+              return {
+                task: "",
+              };
+            });
+          }}
+        >
+          Add
+        </button>
+      </div>
+
+      <TaskList
+        tasks={tasks}
+        editInput={input}
+        setEditInput={setInput}
+        editId={editId}
+        setEditId={setEditId}
+      />
+    </div>
+  );
+};
+
+export default Tasks;
--- src/redux/tasks.js
+++ src/redux/tasks.js
@@ -1,42 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const taskSlice=createSlice({
-    name:'tasks',
-    initialState:[],
-    reducers:{
-        addTask:(state,action)=>{
-           state=[...state,action.payload]
-           return state
-        },
-        removeTask:(state,action)=>{
-            state=state.filter((e)=>e.id!==action.payload.id)
-            return state
-        },
-        updateTask:(state,action)=>{
-            const update=state.map((e)=>{
-            if (e.id===action.payload.id) {
-                return {
-                    ...e,
-                    task:action.payload.task,
-                    completed:action.payload.completed,
-                    isEdited:action.payload.isEdited
-                }
-            }
-            if(action.payload.isEdited===true){
-                return {
-                    ...e,
-                    isEdited:false
-                }
-            }
-            
-            return e
-        })
-        state=update
-        return state
+const taskSlice = createSlice({
+  name: "tasks",
+  initialState: [],
+  reducers: {
+    addTask: (state, action) => {
+      state = [...state, action.payload];
+      return state;
+    },
+    removeTask: (state, action) => {
+      state = state.filter((e) => e.id !== action.payload.id);
+      return state;
+    },
+    updateTask: (state, action) => {
+      const update = state.map((e) => {
+        if (e.id === action.payload.id) {
+          return {
+            ...e,
+            task: action.payload.task,
+            completed: action.payload.completed,
+          };
         }
-    }
-})
+        return e;
+      });
+      state = update;
+      return state;
+    },
+  },
+});
 
-
-export const {addTask,removeTask,updateTask}=taskSlice.actions
-export default taskSlice.reducer 
+export const { addTask, removeTask, updateTask } = taskSlice.actions;
+export default taskSlice.reducer;
